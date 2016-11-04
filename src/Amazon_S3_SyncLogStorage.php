6795009cfbeb7928e10b66e139106ef69bd91922
Marc S. Brooks <marc@mbrooks.info>
2016-11-04 03:49:14 +0000

    Added new and updated existing custom log methods

--- src/Amazon_S3_SyncLogStorage.php
+++ src/Amazon_S3_SyncLogStorage.php
@@ -28,7 +28,27 @@ class Amazon_S3_SyncLogStorage implements Amazon_S3_SyncLogStorageInterface {
   /**
    * {@inheritdoc}
    */
-  public function delete($id) {
+  public function insert($command, $output, $start_time, $end_time) {
+
+    // Strip S3 account information.
+    $command = preg_replace('/--(?:access|secret)_key\s+[\w\+]+\s+/', '', $command);
+
+    $result = $this->connection->insert('amazon_s3_sync_log')
+      ->fields(
+        array(
+          'command' => $command,
+          'output' => $output,
+          'started' => $start_time,
+          'ended' => $end_time,
+        )
+      )->execute();
+    return (bool) $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function purge() {
     $result = $this->connection->truncate('amazon_s3_sync_log')
       ->execute();
     return (bool) $result;
--- src/Amazon_S3_SyncLogStorageInterface.php
+++ src/Amazon_S3_SyncLogStorageInterface.php
@@ -13,10 +13,27 @@ namespace Drupal\amazon_s3_sync;
 interface Amazon_S3_SyncLogStorageInterface {
 
   /**
+   * Insert a log entry into the database. Excludes S3 account information.
+   *
+   * @param string $command
+   *   The S3cmd command that was executed.
+   * @param string $output
+   *   The command result (verbose must be enabled).
+   * @param string $start_time
+   *   The command execution start time.
+   * @param string $end_time
+   *   The command execution end time.
+   *
+   * @return bool
+   *   TRUE if success, FALSE if not.
+   */
+  public function insert($command, $output, $start_time, $end_time);
+
+  /**
    * Remove all logs from the database.
    *
    * @return bool
    *   TRUE if success, FALSE if not.
    */
-  public function delete();
+  public function purge();
 }
